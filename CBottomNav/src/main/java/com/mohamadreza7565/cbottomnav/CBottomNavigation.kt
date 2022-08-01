4dd483a0bb05278f332d2de313eea1d65e488a51
Mohammadreza <engineer.it93@gmail.com>
2022-08-01 16:34:38 +0430

    Add attr for custom view

--- CBottomNav/src/main/java/com/mohamadreza7565/cbottomnav/CBottomNavigation.kt
+++ CBottomNav/src/main/java/com/mohamadreza7565/cbottomnav/CBottomNavigation.kt
@@ -5,10 +5,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import android.view.*
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.view.menu.MenuBuilder
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.forEachIndexed
 
@@ -20,6 +20,12 @@ class CBottomNavigation(
 ) :
     LinearLayout(mContext, attr) {
 
+    private var textShadowColor: Int = 0
+    private var textColor: Int = 0
+    private var iconTint: Int = 0
+    private var badgeTextColor: Int = 0
+    private var badgeBackground: Int = 0
+    private var textSize: Int = 0
     private var mTag: Int? = null
     private lateinit var menu: Menu
     var selectedItemId: Int = 0
@@ -57,6 +63,25 @@ class CBottomNavigation(
 
         val a = context.obtainStyledAttributes(attr, R.styleable.CustomBottomNav)
         val menuId = a.getResourceId(R.styleable.CustomBottomNav_menuChildView, 0)
+        val background =
+            a.getResourceId(R.styleable.CustomBottomNav_background, R.drawable.bg_bottom_nav)
+        linearLayout!!.setBackgroundResource(background)
+
+
+        textColor =
+            a.getResourceId(R.styleable.CustomBottomNav_textColor, R.color.black)
+        iconTint =
+            a.getResourceId(R.styleable.CustomBottomNav_iconTint, R.color.black)
+        badgeTextColor =
+            a.getResourceId(R.styleable.CustomBottomNav_badgeTextColor, R.color.white)
+        badgeBackground =
+            a.getResourceId(R.styleable.CustomBottomNav_badgeBackground, R.drawable.bg_badge)
+        textShadowColor =
+            a.getResourceId(R.styleable.CustomBottomNav_textShadowColor, R.color.white)
+        textSize =
+            a.getDimensionPixelSize(R.styleable.CustomBottomNav_textSize, 0)
+
+
         if (menuId != 0) {
             try {
                 menu = MenuBuilder(context)
@@ -70,6 +95,13 @@ class CBottomNavigation(
 
     }
 
+    fun setSelectedTab(menuId: Int, onClick: () -> Unit) {
+        val tabItem = findViewWithTag<View>(menuId)
+        tabItem.setOnClickListener {
+            onClick.invoke()
+        }
+    }
+
 
     private fun createTabs() {
         menu.forEachIndexed { index, tab ->
@@ -80,33 +112,51 @@ class CBottomNavigation(
             ).inflate(R.layout.item_bottom_bar, null)
 
             inflate.tag = tab.itemId
-            (inflate.findViewById<View>(R.id.ivIcon) as AppCompatImageView).setImageDrawable(
-                tab.icon
+
+            val icon = (inflate.findViewById<View>(R.id.ivIcon) as ImageView)
+            icon.setImageDrawable(tab.icon)
+            icon.setColorFilter(context.resources.getColor(iconTint))
+
+            val title = (inflate.findViewById(R.id.tvTitle) as TextView)
+            title.text = tab.title
+            title.setTextColor(context.resources.getColor(textColor))
+            title.setShadowLayer(
+                10f,
+                1f,
+                1f,
+                context.resources.getColor(textShadowColor)
             )
-            (inflate.findViewById(R.id.tvTitle) as AppCompatTextView).text = tab.title
-            (inflate.findViewById(R.id.tvBadge) as AppCompatTextView).visibility = View.GONE
+            if (textSize!=0)
+                title.textSize = textSize.toFloat()
+
+            val badge = (inflate.findViewById(R.id.tvBadge) as TextView)
+            badge.setTextColor(context.resources.getColor(badgeTextColor))
+            badge.setBackgroundResource(badgeBackground)
+            badge.visibility = View.GONE
+
             inflate.setOnClickListener { v: View ->
                 setSelectedTab(
                     tab
                 )
             }
+
             layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
             layoutParams.setMargins(margin, margin, margin, margin)
             layoutParams.weight = 1.0f
             linearLayout!!.addView(inflate, 0, layoutParams)
             if (index == 0) {
-                makeViolet(inflate)
+                selectItem(inflate)
                 mTag = tab.itemId
                 selectedItemId = tab.itemId
             } else {
-                makeGray(inflate)
+                unSelectItem(inflate)
             }
         }
     }
 
     fun setBadgeCount(badge: Int, menuId: Int) {
         val tabItem = findViewWithTag<View>(menuId)
-        val badgeTv = (tabItem.findViewById<View>(R.id.tvBadge) as AppCompatTextView)
+        val badgeTv = (tabItem.findViewById<View>(R.id.tvBadge) as TextView)
         badgeTv.text = badge.toString()
     }
 
@@ -120,10 +170,10 @@ class CBottomNavigation(
     }
 
     fun setSelectedTab(menuItem: MenuItem) {
-        makeViolet(findViewWithTag(menuItem.itemId))
+        selectItem(findViewWithTag(menuItem.itemId))
         if (menuItem.itemId != mTag) {
             if (mTag != null) {
-                makeGray(findViewWithTag(mTag))
+                unSelectItem(findViewWithTag(mTag))
             }
             mTag = menuItem.itemId
             selectedItemId = menuItem.itemId
@@ -137,11 +187,11 @@ class CBottomNavigation(
         }
     }
 
-    private fun makeViolet(view: View) {
+    private fun selectItem(view: View) {
         view.findViewById<MotionLayout>(R.id.bottomNavMotionLyt).transitionToStart()
     }
 
-    private fun makeGray(view: View) {
+    private fun unSelectItem(view: View) {
         view.findViewById<MotionLayout>(R.id.bottomNavMotionLyt).transitionToEnd()
     }
 
